5b4871f6c69bb982bac02159cd28fc0c5c1cd08f
mananshah511 <48170891+mananshah511@users.noreply.github.com>
2020-08-03 17:31:13 +0530

    Add files via upload

---
+++ 700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        //cout<<root->val<<" "<<val<<endl;
+        if((root->val)==val)
+        {
+            return root;
+        }
+        TreeNode* l=searchBST(root->left,val);
+        TreeNode* r=searchBST(root->right,val);
+        if(l!=NULL)
+        {
+            return l;
+        }
+        if(r!=NULL)
+        {
+            return r;
+        }
+        return NULL;
+    }
+};
